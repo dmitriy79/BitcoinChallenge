0762b6efdc8698dea87dc8d9e1f0965cb6539aa5
Matthew Kragness <50250493+mkragness@users.noreply.github.com>
2021-01-09 15:04:21 -0800

    cleaned up code

--- btcchallenge.py
+++ btcchallenge.py
@@ -1,24 +1,24 @@
-from bit import *
-from bit.format import bytes_to_wif
-from random import *
+from bit import Key
+from random import randint
 import sys
+import time 
 
 challengearg = sys.argv[1]
 challengeint = int(challengearg)
-print('Challenge Number = ',challengeint)
+print('Challenge Number = ', challengeint)
 addressfile = open("addresslist.txt", "r")
 challenge = addressfile.readlines()
-challengeaddress = challenge[challengeint -1]
+challengeaddress = challenge[challengeint - 1]
 challengeaddress = challengeaddress.strip()
-print('Challenge Address: ',challenge[challengeint -1])
+print('Challenge Address: ', challenge[challengeint - 1])
 addressfile.close()
 
 maxfile = open("maxlist.txt", "r")
 maxlimit = maxfile.readlines()
-maxilimit = int(maxlimit[challengeint -1])
-lowlimit = int(maxlimit[challengeint -2])
-print('Range Lower Limit: ',lowlimit)
-print('Range Upper Limit: ',maxilimit)
+maxilimit = int(maxlimit[challengeint - 1])
+lowlimit = int(maxlimit[challengeint - 2])
+print('Range Lower Limit: ', lowlimit)
+print('Range Upper Limit: ', maxilimit)
 maxfile.close()
 
 pkey = 0
@@ -31,33 +31,38 @@ print('CHALLENGE ACCEPTED')
 print('searching.........')
 
 while True:
-    scantime +=1
-    pkey= randint(lowlimit,maxilimit)
+    if scantime == 0:
+        starttime = time.time()
+    scantime = scantime + plus1
+    pkey = randint(lowlimit, maxilimit)
     hexpkey = hex(pkey)
-    hexpkey = hexpkey [2:]
+    hexpkey = hexpkey[2:]
     prvstring = hexpkey
     prvstring = str(prvstring)
     prvstring = prvstring.zfill(64)
     key = Key.from_hex(prvstring)
-    pubaddresscomp = str(key.address)    
+    pubaddresscomp = str(key.address) 
+    endtime = time.time() - starttime 
     
     if pubaddresscomp == hunt:
         print(pubaddresscomp)
         data = open("btclist.txt", "w")
-        print (prvstring,file = data)
-        print (pubaddresscomp, file = data)
+        print(prvstring, file=data)
+        print(pubaddresscomp, file=data)
         data.close()
         break
     
-    if scantime == 100000:
+    if endtime == 60:
+        print(scantime, 'keys per minute')
         totalscantime = totalscantime + scantime
+        print(totalscantime, 'keys searched')
         scantime = 0
-        print(totalscantime,'keys searched')
-    
+
 print("Bitcoin Found")
 print(pubaddresscomp)
 print(prvstring)
 print(totalscantime)
-status = open("nohupstatus.txt","a")
-print("coin found closed",file = status)
+status = open("nohupstatus.txt", "a")
+print("coin found closed", file=status)
 status.close()
+
